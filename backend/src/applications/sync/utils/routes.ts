7eb3c872a1e0832e350f1a2cdd23cf320a54a3d0
johaven <johan@sync-in.com>
2025-08-26 01:57:55 +0200

    chore(backend:sync): use object instead of enum to check repository

--- backend/src/applications/sync/utils/routes.ts
+++ backend/src/applications/sync/utils/routes.ts
@@ -5,12 +5,12 @@
  */
 
 import { PATH_TO_SPACE_SEGMENTS } from '../../spaces/utils/routes'
-import { SYNC_PATH_REPOSITORY, SYNC_REPOSITORY } from '../constants/sync'
+import { SYNC_PATH_REPOSITORY } from '../constants/sync'
 
 export function SYNC_PATH_TO_SPACE_SEGMENTS(path: string): string[] {
   const urlSegments = PATH_TO_SPACE_SEGMENTS(path)
   const repository = urlSegments.shift()
-  if (!(repository in SYNC_REPOSITORY)) {
+  if (!(repository in SYNC_PATH_REPOSITORY)) {
     throw new Error(`Repository not found : ${repository}`)
   }
   urlSegments.unshift(...SYNC_PATH_REPOSITORY[repository])
