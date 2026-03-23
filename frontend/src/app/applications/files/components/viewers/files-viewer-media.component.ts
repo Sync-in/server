d9e81f02dc5fe40151471e5eed4624f4de030703
johaven <johan@sync-in.com>
2026-03-23 23:14:47 +0100

    fix(files): encode special characters not handled by AuthInterceptor

--- frontend/src/app/applications/files/components/viewers/files-viewer-media.component.ts
+++ frontend/src/app/applications/files/components/viewers/files-viewer-media.component.ts
@@ -3,6 +3,7 @@ import { VgBufferingModule } from '@videogular/ngx-videogular/buffering'
 import { VgControlsModule } from '@videogular/ngx-videogular/controls'
 import { VgCoreModule } from '@videogular/ngx-videogular/core'
 import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play'
+import { FileModel } from '../../models/file.model'
 
 @Component({
   selector: 'app-files-viewer-media',
@@ -11,12 +12,12 @@ import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play'
   template: `
     <vg-player [style.height.px]="currentHeight()">
       <video [vgMedia]="$any(media)" #media preload="none" autoplay controls>
-        <source [src]="fileUrl()" type="video/webm" />
+        <source [src]="file().dataUrl" type="video/webm" />
       </video>
     </vg-player>
   `
 })
 export class FilesViewerMediaComponent {
-  fileUrl = input<string>()
+  file = input.required<FileModel>()
   currentHeight = input<number>()
 }
--- frontend/src/app/applications/files/components/viewers/files-viewer-pdf.component.ts
+++ frontend/src/app/applications/files/components/viewers/files-viewer-pdf.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core'
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
 import { assetsUrl } from '../../files.constants'
+import { FileModel } from '../../models/file.model'
 
 @Component({
   selector: 'app-files-viewer-pdf',
@@ -8,11 +9,11 @@ import { assetsUrl } from '../../files.constants'
   template: ` <iframe [src]="url()" class="app-viewer-iframe" [style.height.px]="currentHeight()"></iframe> `
 })
 export class FilesViewerPdfComponent {
-  fileUrl = input<string>()
+  file = input.required<FileModel>()
   currentHeight = input<number>()
   private readonly sanitizer = inject(DomSanitizer)
   private readonly pdfjsUrl = `${assetsUrl}/pdfjs/web/viewer.html?file=`
   url = computed<SafeResourceUrl | null>(() =>
-    this.fileUrl() ? this.sanitizer.bypassSecurityTrustResourceUrl(`${this.pdfjsUrl}${encodeURIComponent(this.fileUrl())}`) : null
+    this.file() ? this.sanitizer.bypassSecurityTrustResourceUrl(`${this.pdfjsUrl}${this.file().dataUrl}`) : null
   )
 }
