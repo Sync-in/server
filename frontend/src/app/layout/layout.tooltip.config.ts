a930f4c3012df88c7e2bf1701c7df8fc7d62766d
johaven <johan@sync-in.com>
2026-03-24 23:48:19 +0100

    chore(frontend): normalize tooltip containers for app top menus & adjust badge line height & actions column in `sync-transfers.component.ts`

--- frontend/src/app/layout/layout.tooltip.config.ts
+++ frontend/src/app/layout/layout.tooltip.config.ts
@@ -2,6 +2,7 @@ import { TooltipConfig } from 'ngx-bootstrap/tooltip'
 
 export function getToolTipConfig(): TooltipConfig {
   return Object.assign(new TooltipConfig(), {
+    container: '.wrapper',
     adaptivePosition: false,
     triggers: 'hover',
     delay: 600
